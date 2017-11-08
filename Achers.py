dfb55788450ff1e7532aabf69c9138adc9602ed1
s203 <s203@AI241LAB0203M11.local>
2017-11-08 11:54:10 -0800

    update

---
+++ Achers.py
@@ -0,0 +1,4 @@
+print("how many achers do you want")
+Achers = int(input())
+miles = Achers * 0.0015625
+print("that is", miles, "miles")
---
+++ volumeconverter.py
@@ -0,0 +1,5 @@
+print("hello, how many gallons would you like to convert to liters?")
+gal = int(input())
+
+liters = gal * .2641
+print("there are", liters, "liters")
